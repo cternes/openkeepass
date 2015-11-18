1114a169e9332adedeff2eb71851ddb2705c8243
Guillaume Pichenot <guillaume.pichenot@cgi.com>
2015-11-18 18:00:57 +0100

    Fixed dangling FileInputStream

--- src/main/java/de/slackspace/openkeepass/KeePassDatabase.java
+++ src/main/java/de/slackspace/openkeepass/KeePassDatabase.java
@@ -105,10 +105,20 @@ public class KeePassDatabase {
 			throw new IllegalArgumentException("You must provide a valid KeePass database file.");
 		}
 		
+		InputStream keePassDatabaseStream = null;
 		try {
-			return getInstance(new FileInputStream(keePassDatabaseFile));
+			keePassDatabaseStream = new FileInputStream(keePassDatabaseFile);
+			return getInstance(keePassDatabaseStream);
 		} catch (FileNotFoundException e) {
 			throw new IllegalArgumentException("The KeePass database file could not be found. You must provide a valid KeePass database file.");
+		} finally {
+		  if (keePassDatabaseStream != null) {
+		    try {
+		      keePassDatabaseStream.close();
+		    } catch (IOException e) {
+		      // Ignore
+		    }
+		  }
 		}
 	}
 	
