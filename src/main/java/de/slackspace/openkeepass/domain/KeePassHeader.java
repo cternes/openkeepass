519a8aba9ec1382745c7d50796a20cc233b727d2
cternes <github@slackspace.de>
2015-12-03 12:02:38 +0100

    Implemented writing of header

--- src/main/java/de/slackspace/openkeepass/domain/KeePassHeader.java
+++ src/main/java/de/slackspace/openkeepass/domain/KeePassHeader.java
@@ -1,10 +1,28 @@
 package de.slackspace.openkeepass.domain;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
+import de.slackspace.openkeepass.util.ByteUtils;
+
 public class KeePassHeader {
 
+	public static final int CIPHER = 2; 
+	public static final int COMPRESSION = 3; 
+	public static final int MASTER_SEED = 4; 
+	public static final int TRANSFORM_SEED = 5; 
+	public static final int TRANSFORM_ROUNDS = 6; 
+	public static final int ENCRYPTION_IV = 7; 
+	public static final int PROTECTED_STREAM_KEY = 8; 
+	public static final int STREAM_START_BYTES = 9; 
+	public static final int INNER_RANDOM_STREAM_ID = 10;
+	
+	private static final byte[] DATABASE_V2_FILE_SIGNATURE_1 = ByteUtils.hexStringToByteArray("03d9a29a");
+	private static final byte[] DATABASE_V2_FILE_SIGNATURE_2 = ByteUtils.hexStringToByteArray("67fb4bb5");
+	private static final byte[] DATABASE_V2_FILE_VERSION = ByteUtils.hexStringToByteArray("00000300");
+	
 	private int headerSize = 0;
 	private byte[] cipher;
 	private byte[] encryptionIV;
@@ -18,31 +36,93 @@ public class KeePassHeader {
 
 	public void setValue(int headerId, byte[] value) {
 		switch (headerId) {
-			case 2:	setCipher(value);
+			case CIPHER:	setCipher(value);
 			break;
-			case 3: setCompressionFlag(value);
+			case COMPRESSION: setCompressionFlag(value);
 			break;
-			case 4: setMasterSeed(value);
+			case MASTER_SEED: setMasterSeed(value);
 			break;
-			case 5: setTransformSeed(value);
+			case TRANSFORM_SEED: setTransformSeed(value);
 			break;
-			case 6: setTransformRounds(value);
+			case TRANSFORM_ROUNDS: setTransformRounds(value);
 			break;
-			case 7: setEncryptionIV(value);
+			case ENCRYPTION_IV: setEncryptionIV(value);
 			break;
-			case 8: setProtectedStreamKey(value);
+			case PROTECTED_STREAM_KEY: setProtectedStreamKey(value);
 			break;
-			case 9: setStreamStartBytes(value);
+			case STREAM_START_BYTES: setStreamStartBytes(value);
 			break;
-			case 10: setInnerRandomStreamId(value);
+			case INNER_RANDOM_STREAM_ID: setInnerRandomStreamId(value);
 			break;
 		}
+	}
+
+	public byte[] getBytes() {
+		try {
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			
+			stream.write(DATABASE_V2_FILE_SIGNATURE_1);
+			stream.write(DATABASE_V2_FILE_SIGNATURE_2);
+			stream.write(DATABASE_V2_FILE_VERSION);
+			
+			for (int i = 2; i < 11; i++) {
+				byte[] headerValue = getValue(i);
+				
+				// Write index
+				stream.write(i);
+				
+				// Write length
+				byte[] length = new byte[] { (byte) headerValue.length, 0 }; 
+				stream.write(length);
+				
+				// Write value
+				stream.write(headerValue);
+			}
+			
+			// Write terminating flag
+			stream.write(getEndOfHeader());
+			
+			return stream.toByteArray();
+		} catch (IOException e) {
+			throw new RuntimeException("Could not write header value to stream", e);
+		}
+	}
+	
+	private byte[] getEndOfHeader() throws IOException {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		stream.write(0);
+		stream.write(4);
+		stream.write(0);
+		stream.write("\r\n\r\n".getBytes());
+		
+		return stream.toByteArray();
+	}
+
+	public byte[] getValue(int headerId) {
+		switch (headerId) {
+			case CIPHER:	return getCipher();
+			case COMPRESSION: return getCompressionFlag();
+			case MASTER_SEED: return getMasterSeed();
+			case TRANSFORM_SEED: return getTransformSeed();
+			case TRANSFORM_ROUNDS: return getTransformRoundsByte();
+			case ENCRYPTION_IV: return getEncryptionIV();
+			case PROTECTED_STREAM_KEY: return getProtectedStreamKey();
+			case STREAM_START_BYTES: return getStreamStartBytes();
+			case INNER_RANDOM_STREAM_ID: return getInnerRandomStreamId();
+		}
+		
+		return new byte[0];
 	}
 
 	private void setInnerRandomStreamId(byte[] value) {
 		ByteBuffer buffer = wrapInBuffer(value);
 		int intValue = buffer.getInt();
 		crsAlgorithm = CrsAlgorithm.parseValue(intValue);
+	}
+	
+	private byte[] getInnerRandomStreamId() {
+		int intValue = CrsAlgorithm.getIntValue(crsAlgorithm);
+		return wrapInBuffer(intValue);
 	}
 
 	private void setStreamStartBytes(byte[] value) {
@@ -59,7 +139,7 @@ public class KeePassHeader {
 
 	private void setTransformRounds(byte[] value) {
 		ByteBuffer buffer = wrapInBuffer(value);
-		transformRounds  = buffer.getLong();
+		transformRounds = buffer.getLong();
 	}
 
 	private void setTransformSeed(byte[] value) {
@@ -75,6 +155,15 @@ public class KeePassHeader {
 		int intValue = buffer.getInt();
 		
 		compression = CompressionAlgorithm.parseValue(intValue);
+	}
+	
+	public void setCompression(CompressionAlgorithm algorithm) {
+		compression = algorithm;
+	}
+	
+	private byte[] getCompressionFlag() {
+		int intValue = CompressionAlgorithm.getIntValue(compression);
+		return wrapInBuffer(intValue);
 	}
 
 	private void setCipher(byte[] value) {
@@ -95,6 +184,14 @@ public class KeePassHeader {
 	public long getTransformRounds() {
 		return transformRounds;
 	}
+	
+	public void setTransformRounds(long rounds) {
+		transformRounds = rounds;
+	}
+	
+	private byte[] getTransformRoundsByte() {
+		return wrapInBuffer(transformRounds);
+	}
 
 	public byte[] getEncryptionIV() {
 		return encryptionIV;
@@ -106,6 +203,10 @@ public class KeePassHeader {
 
 	public CrsAlgorithm getCrsAlgorithm() {
 		return crsAlgorithm;
+	}
+	
+	public void setCrsAlgorithm(CrsAlgorithm algorithm) {
+		crsAlgorithm = algorithm;
 	}
 
 	public byte[] getMasterSeed() {
@@ -132,5 +233,19 @@ public class KeePassHeader {
 		ByteBuffer buffer = ByteBuffer.wrap(value);
 		buffer.order(ByteOrder.LITTLE_ENDIAN);
 		return buffer;
+	}
+	
+	private byte[] wrapInBuffer(int value) {
+		ByteBuffer buffer = ByteBuffer.allocate(4);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.putInt(value);
+		return buffer.array();
+	}
+	
+	private byte[] wrapInBuffer(long value) {
+		ByteBuffer buffer = ByteBuffer.allocate(8);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		buffer.putLong(value);
+		return buffer.array();
 	}
 }
--- src/test/java/de/slackspace/openkeepass/domain/KeepassHeaderTest.java
+++ src/test/java/de/slackspace/openkeepass/domain/KeepassHeaderTest.java
@@ -68,4 +68,21 @@ public class KeepassHeaderTest {
 		keepassHeader.setValue(headerId, value);
 		return keepassHeader;
 	}
+	
+	@Test
+	public void whenGetBytesIsCalledShouldReturnHeaderBytesCorrectly() {
+		KeePassHeader header = new KeePassHeader();
+		header.setValue(KeePassHeader.MASTER_SEED, ByteUtils.hexStringToByteArray("35ac8b529bc4f6e44194bccd0537fcb433a30bcb847e63156262c4df99c528ca"));
+		header.setValue(KeePassHeader.TRANSFORM_SEED, ByteUtils.hexStringToByteArray("0d52d93efc5493ae6623f0d5d69bb76bd976bb717f4ee67abbe43528ebfbb646"));
+		header.setTransformRounds(8000);
+		header.setValue(KeePassHeader.ENCRYPTION_IV, ByteUtils.hexStringToByteArray("2c605455f181fbc9462aefb817852b37"));
+		header.setValue(KeePassHeader.STREAM_START_BYTES, ByteUtils.hexStringToByteArray("69d788d9b01ea1facd1c0bf0187e7d74e4aa07b20d464f3d23d0b2dc2f059ff8"));
+		header.setValue(KeePassHeader.CIPHER, ByteUtils.hexStringToByteArray("31C1F2E6BF714350BE5805216AFC5AFF"));
+		header.setValue(KeePassHeader.PROTECTED_STREAM_KEY, ByteUtils.hexStringToByteArray("ec77a2169769734c5d26e5341401f8d7b11052058f8455d314879075d0b7e257"));
+		header.setCompression(CompressionAlgorithm.Gzip);
+		header.setCrsAlgorithm(CrsAlgorithm.Salsa20);
+		
+		Assert.assertEquals("03d9a29a67fb4bb50000030002100031c1f2e6bf714350be5805216afc5aff0304000100000004200035ac8b529bc4f6e44194bccd0537fcb433a30bcb847e63156262c4df99c528ca0520000d52d93efc5493ae6623f0d5d69bb76bd976bb717f4ee67abbe43528ebfbb646060800401f0000000000000710002c605455f181fbc9462aefb817852b37082000ec77a2169769734c5d26e5341401f8d7b11052058f8455d314879075d0b7e25709200069d788d9b01ea1facd1c0bf0187e7d74e4aa07b20d464f3d23d0b2dc2f059ff80a0400020000000004000d0a0d0a", ByteUtils.toHexString(header.getBytes()));
+	}
+	
 }
