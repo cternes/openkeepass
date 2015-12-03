4db3e1af2e1eb470cee9a3aa9b1a9acbb20c5ebb
cternes <github@slackspace.de>
2015-12-03 10:30:50 +0100

    Refactored HashedBlockOutputStream

--- src/main/java/de/slackspace/openkeepass/stream/HashedBlockOutputStream.java
+++ src/main/java/de/slackspace/openkeepass/stream/HashedBlockOutputStream.java
@@ -7,47 +7,34 @@ import java.security.NoSuchAlgorithmException;
 
 public class HashedBlockOutputStream extends OutputStream {
 
-private final static int DEFAULT_BUFFER_SIZE = 1024 * 1024; 
-	
-	private LittleEndianOutputStream baseStream;
+	private final static int DEFAULT_BUFFER_SIZE = 1024 * 1024;
+
+	private OutputStream baseStream;
 	private int bufferPos = 0;
 	private byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];;
 	private long bufferIndex = 0;
-	
+
 	public HashedBlockOutputStream(OutputStream os) {
-		init(os, DEFAULT_BUFFER_SIZE);
+		baseStream = os;
 	}
-	
-	public HashedBlockOutputStream(OutputStream os, int bufferSize) {
-		if ( bufferSize <= 0 ) {
-			bufferSize = DEFAULT_BUFFER_SIZE;
-		}
-		
-		init(os, bufferSize);
-	}
-	
-	private void init(OutputStream os, int bufferSize) {
-		baseStream = new LittleEndianOutputStream(os);
-		buffer = new byte[bufferSize];
-	}
-	
+
 	@Override
 	public void write(int oneByte) throws IOException {
 		byte[] buf = new byte[1];
-		buf[0] = (byte)oneByte;
+		buf[0] = (byte) oneByte;
 		write(buf, 0, 1);
 	}
 
 	@Override
 	public void close() throws IOException {
-		if ( bufferPos != 0 ) {
+		if (bufferPos != 0) {
 			// Write remaining buffered amount
-			WriteHashedBlock();
+			writeHashedBlock();
 		}
-		
+
 		// Write terminating block
-		WriteHashedBlock();
-		
+		writeHashedBlock();
+
 		flush();
 		baseStream.close();
 	}
@@ -59,69 +46,103 @@ private final static int DEFAULT_BUFFER_SIZE = 1024 * 1024;
 
 	@Override
 	public void write(byte[] b, int offset, int count) throws IOException {
-		while ( count > 0 ) {
-			if ( bufferPos == buffer.length ) {
-				WriteHashedBlock();
+		while (count > 0) {
+			if (bufferPos == buffer.length) {
+				writeHashedBlock();
 			}
-			
+
 			int copyLen = Math.min(buffer.length - bufferPos, count);
-			
+
 			System.arraycopy(b, offset, buffer, bufferPos, copyLen);
-			
+
 			offset += copyLen;
 			bufferPos += copyLen;
-			
+
 			count -= copyLen;
 		}
 	}
 
-	private void WriteHashedBlock() throws IOException {
-		baseStream.writeUInt(bufferIndex);
+	private void writeHashedBlock() throws IOException {
+		writeUInt(bufferIndex);
 		bufferIndex++;
-		
-		if ( bufferPos > 0 ) {
+
+		if (bufferPos > 0) {
 			MessageDigest md = null;
 			try {
 				md = MessageDigest.getInstance("SHA-256");
 			} catch (NoSuchAlgorithmException e) {
 				throw new IOException("SHA-256 not implemented here.");
 			}
-			
+
 			byte[] hash;
 			md.update(buffer, 0, bufferPos);
 			hash = md.digest();
-			/*
-			if ( bufferPos == buffer.length) {
-				hash = md.digest(buffer);
-			} else {
-				byte[] b = new byte[bufferPos];
-				System.arraycopy(buffer, 0, b, 0, bufferPos);
-				hash = md.digest(b);
-			}
-			*/
-			
+
 			baseStream.write(hash);
 
 		} else {
 			// Write 32-bits of zeros
-			baseStream.writeLong(0L);
-			baseStream.writeLong(0L);
-			baseStream.writeLong(0L);
-			baseStream.writeLong(0L);
+			writeLong(0L);
+			writeLong(0L);
+			writeLong(0L);
+			writeLong(0L);
 		}
-		
-		baseStream.writeInt(bufferPos);
-		
-		if ( bufferPos > 0 ) {
+
+		writeInt(bufferPos);
+
+		if (bufferPos > 0) {
 			baseStream.write(buffer, 0, bufferPos);
 		}
-		
+
 		bufferPos = 0;
-		
+
 	}
 
 	@Override
 	public void write(byte[] buffer) throws IOException {
 		write(buffer, 0, buffer.length);
+	}
+
+	public void writeUInt(long uint) throws IOException {
+		baseStream.write(writeIntBuf((int) uint));
+	}
+
+	public void writeInt(int val) throws IOException {
+		byte[] buf = new byte[4];
+		writeInt(val, buf, 0);
+
+		baseStream.write(buf);
+	}
+
+	public byte[] writeIntBuf(int val) {
+		byte[] buf = new byte[4];
+		writeInt(val, buf, 0);
+
+		return buf;
+	}
+
+	public void writeInt(int val, byte[] buf, int offset) {
+		buf[offset + 0] = (byte) (val & 0xFF);
+		buf[offset + 1] = (byte) ((val >>> 8) & 0xFF);
+		buf[offset + 2] = (byte) ((val >>> 16) & 0xFF);
+		buf[offset + 3] = (byte) ((val >>> 24) & 0xFF);
+	}
+
+	public void writeLong(long val) throws IOException {
+		byte[] buf = new byte[8];
+
+		writeLong(val, buf, 0);
+		baseStream.write(buf);
+	}
+
+	public void writeLong(long val, byte[] buf, int offset) {
+		buf[offset + 0] = (byte) (val & 0xFF);
+		buf[offset + 1] = (byte) ((val >>> 8) & 0xFF);
+		buf[offset + 2] = (byte) ((val >>> 16) & 0xFF);
+		buf[offset + 3] = (byte) ((val >>> 24) & 0xFF);
+		buf[offset + 4] = (byte) ((val >>> 32) & 0xFF);
+		buf[offset + 5] = (byte) ((val >>> 40) & 0xFF);
+		buf[offset + 6] = (byte) ((val >>> 48) & 0xFF);
+		buf[offset + 7] = (byte) ((val >>> 56) & 0xFF);
 	}
 }
