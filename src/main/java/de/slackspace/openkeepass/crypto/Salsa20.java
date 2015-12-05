8f6c59309b7a7beb48510e02de127d353124cf26
cternes <github@slackspace.de>
2015-12-05 15:15:57 +0100

    Fixed issue-8 by refactoring salsa20 to use BC as provider

--- src/main/java/de/slackspace/openkeepass/crypto/Salsa20.java
+++ src/main/java/de/slackspace/openkeepass/crypto/Salsa20.java
@@ -1,28 +1,38 @@
 package de.slackspace.openkeepass.crypto;
 
 import java.io.UnsupportedEncodingException;
+import java.security.Security;
 
-import org.bouncycastle.crypto.engines.Salsa20Engine;
-import org.bouncycastle.crypto.params.KeyParameter;
-import org.bouncycastle.crypto.params.ParametersWithIV;
+import javax.crypto.Cipher;
+import javax.crypto.ShortBufferException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Base64;
 import org.bouncycastle.util.encoders.Hex;
 
 public class Salsa20 implements ProtectedStringCrypto {
 	
+	private static final String SALSA20 = "Salsa20";
 	private static final String ENCODING = "UTF-8";
 	private static final String SALSA20IV = "E830094B97205D2A";
 	
-	private Salsa20Engine salsa20Engine;
+	private Cipher salsa20Engine;
 	
-	private Salsa20() {	}
+	private Salsa20() {
+		Security.addProvider(new BouncyCastleProvider());
+	}
 	
 	private void initialize(byte[] protectedStreamKey) {
 		byte[] salsaKey = Sha256.hash(protectedStreamKey);
-		salsa20Engine = new Salsa20Engine();
 		
-		// Encrypt or Decrypt does not matter here
-		salsa20Engine.init(true, new ParametersWithIV(new KeyParameter(salsaKey), Hex.decode(SALSA20IV)));
+		try {
+			salsa20Engine = Cipher.getInstance(SALSA20, BouncyCastleProvider.PROVIDER_NAME);
+			salsa20Engine.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(salsaKey, SALSA20), new IvParameterSpec(Hex.decode(SALSA20IV)));
+		} catch (Exception e) {
+			throw new RuntimeException("Could not find provider '" + SALSA20 + "'", e);
+		}
 	}
 	
 	public static Salsa20 createInstance(byte[] protectedStreamKey) {
@@ -44,10 +54,11 @@ public class Salsa20 implements ProtectedStringCrypto {
 		byte[] protectedBuffer = Base64.decode(protectedString.getBytes());
 		byte[] plainText = new byte[protectedBuffer.length];
 		
-		salsa20Engine.processBytes(protectedBuffer, 0, protectedBuffer.length, plainText, 0);
-		
-		try { 
+		try {
+			salsa20Engine.update(protectedBuffer, 0, protectedBuffer.length, plainText, 0);
 			return new String(plainText, ENCODING);
+		} catch (ShortBufferException e) {
+			throw new RuntimeException(e);
 		} catch (UnsupportedEncodingException e) {
 			throw new UnsupportedOperationException("The encoding UTF-8 is not supported", e);
 		}
@@ -62,13 +73,15 @@ public class Salsa20 implements ProtectedStringCrypto {
 			byte[] plainStringBytes = plainString.getBytes(ENCODING);
 			byte[] encodedText = new byte[plainStringBytes.length];
 			
-			salsa20Engine.processBytes(plainStringBytes, 0, plainStringBytes.length, encodedText, 0);
+			salsa20Engine.update(plainStringBytes, 0, plainStringBytes.length, encodedText, 0);
 			
 			byte[] protectedBuffer = Base64.encode(encodedText);
 			
 			return new String(protectedBuffer, ENCODING);
 		} catch (UnsupportedEncodingException e) {
 			throw new UnsupportedOperationException("The encoding UTF-8 is not supported", e);
+		} catch (ShortBufferException e) {
+			throw new RuntimeException(e);
 		}
 	}
 	
