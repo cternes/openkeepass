b8516d1afad42224a4009797858703ce99967ec3
cternes <github@slackspace.de>
2015-12-09 08:29:49 +0100

    Added xml adapter for boolean values

---
+++ src/main/java/de/slackspace/openkeepass/xml/BooleanXmlAdapter.java
@@ -0,0 +1,30 @@
+package de.slackspace.openkeepass.xml;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * This class is a JAXB adapter to transform boolean values to/from xml using JAXB.
+ *
+ */
+public class BooleanXmlAdapter extends XmlAdapter<String, Boolean> {
+
+	/* (non-Javadoc)
+	 * @see javax.xml.bind.annotation.adapters.XmlAdapter#marshal(java.lang.Object)
+	 */
+	@Override
+	public String marshal(Boolean value) throws Exception {
+		if(value == true) {
+			return "True";
+		}
+		return "False";
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.xml.bind.annotation.adapters.XmlAdapter#unmarshal(java.lang.Object)
+	 */
+	@Override
+	public Boolean unmarshal(String value) throws Exception {
+		return value.equalsIgnoreCase("true");
+	}
+
+}
