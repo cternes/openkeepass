c39a77c909f45b0d5afedaee0fde6acbc425260b
cternes <github@slackspace.de>
2016-01-10 15:15:30 +0100

    Added javadoc for KeePassFileBuilder

--- src/main/java/de/slackspace/openkeepass/domain/KeePassFileBuilder.java
+++ src/main/java/de/slackspace/openkeepass/domain/KeePassFileBuilder.java
@@ -2,6 +2,14 @@ package de.slackspace.openkeepass.domain;
 
 import de.slackspace.openkeepass.domain.zipper.GroupZipper;
 
+/**
+ * A builder to create {@link KeePassFile}s.
+ * <p>
+ * Can be used to create a completely new {@link KeePassFile} or to modify an existing one.
+ * <p>
+ * To modify an existing one use the {@link GroupZipper}.
+ *
+ */
 public class KeePassFileBuilder {
 
 	Meta meta;
@@ -10,6 +18,11 @@ public class KeePassFileBuilder {
 	private GroupBuilder topGroupBuilder = new GroupBuilder();
 	private KeePassFile keePassFile;
 	
+	/**
+	 * Creates a builder and initializes it with the structure from the given KeePass file. 
+	 * 
+	 * @param keePassFile the KeePass file which will be used to initialize the builder
+	 */
 	public KeePassFileBuilder(KeePassFile keePassFile) {
 		this.keePassFile = keePassFile;
 		this.meta = keePassFile.getMeta();
@@ -17,12 +30,22 @@ public class KeePassFileBuilder {
 		rootBuilder = new GroupBuilder(keePassFile.getRoot());
 	}
 	
+	/**
+	 * Creates a builder with the given databasename.
+	 * 
+	 * @param databaseName the name of the database
+	 */
 	public KeePassFileBuilder(String databaseName) {
 		meta = new MetaBuilder(databaseName)
 				.historyMaxItems(10)
 				.build();
 	}
 	
+	/**
+	 * Creates a builder with the given meta object.
+	 * 
+	 * @param meta the meta object to initialize the builder meta
+	 */
 	public KeePassFileBuilder(Meta meta) {
 		this.meta = meta;
 	}
@@ -32,6 +55,12 @@ public class KeePassFileBuilder {
 		return this;
 	}
 
+	/**
+	 * Adds the given groups right under the root node.
+	 * 
+	 * @param groups the groups which should be added
+	 * @return
+	 */
 	public KeePassFileBuilder addTopGroups(Group... groups) {
 		for (Group group : groups) {
 			rootBuilder.addGroup(group);
@@ -40,6 +69,12 @@ public class KeePassFileBuilder {
 		return this;
 	}
 	
+	/**
+	 * Add the given entries right under the root node.
+	 * 
+	 * @param entries the entries which should be added
+	 * @return
+	 */
 	public KeePassFileBuilder addTopEntries(Entry... entries) {
 		for (Entry entry : entries) {
 			topGroupBuilder.addEntry(entry);
@@ -48,6 +83,12 @@ public class KeePassFileBuilder {
 		return this;
 	}
 	
+	/**
+	 * Builds a new KeePass file.
+	 * 
+	 * @return a new KeePass file
+	 * @see KeePassFile
+	 */
 	public KeePassFile build() {
 		setTopGroupNameIfNotExisting();
 		
@@ -56,6 +97,13 @@ public class KeePassFileBuilder {
 		return new KeePassFile(this);
 	}
 	
+	/**
+	 * Returns a {@link GroupZipper} with the structure of the builders {@link KeePassFile} as underlying data.
+	 * <p>
+	 * A GroupZipper can be used to easily modify existing KeePass files. 
+	 * 
+	 * @return a new group zipper
+	 */
 	public GroupZipper getZipper() {
 		return new GroupZipper(keePassFile);
 	}
