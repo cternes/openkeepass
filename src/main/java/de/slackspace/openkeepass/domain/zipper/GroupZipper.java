c5209138a321c6878e7e4a1aea368d086b9d78f6
cternes <github@slackspace.de>
2016-01-10 14:30:08 +0100

    Added javadoc and tests for GroupZipper

--- src/main/java/de/slackspace/openkeepass/domain/zipper/GroupZipper.java
+++ src/main/java/de/slackspace/openkeepass/domain/zipper/GroupZipper.java
@@ -5,6 +5,17 @@ import de.slackspace.openkeepass.domain.KeePassFile;
 import de.slackspace.openkeepass.domain.KeePassFileBuilder;
 import de.slackspace.openkeepass.domain.Meta;
 
+/**
+ * A zipper is used to navigate through a tree structure of {@link Group} objects.
+ * <p>
+ * It is kind of an iterator with a pointer to the current element. The current element can be found with
+ * {@link #getNode()} method.
+ * <p>
+ * Navigation through the tree is possible with the methods {@link #down()} {@link #up()} {@link #left()} {@link #right()}.
+ * <p>
+ * The tree can also be modified by using the {@link #replace(Group)} method to replace a node with another one.
+ *
+ */
 public class GroupZipper {
 
 	private Meta meta;
@@ -12,6 +23,11 @@ public class GroupZipper {
 	private Group node;
 	private GroupZipper parent;
 	
+	/**
+	 * Create a zipper with the tree structure of the given KeePass file.
+	 * 
+	 * @param keePassFile the underlying data structure
+	 */
 	public GroupZipper(KeePassFile keePassFile) {
 		this.meta = keePassFile.getMeta();
 		this.node = keePassFile.getRoot().getGroups().get(0);
@@ -22,6 +38,14 @@ public class GroupZipper {
 		this.node = group;
 	}
 	
+	/**
+	 * Navigates down the tree to the first child node of the current node.
+	 * <p>
+	 * If the current node has no childs an exception will be thrown.
+	 * 
+	 * @return 
+	 * @throws RuntimeException if the current node has no child nodes
+	 */
 	public GroupZipper down() {
 		if(node.getGroups() == null || node.getGroups().isEmpty()) {
 			throw new RuntimeException("Could not move down because this group does not have any children");
@@ -34,6 +58,14 @@ public class GroupZipper {
 		return this;
 	}
 	
+	/**
+	 * Navigates up the tree to the parent node of the current node.
+	 * <p>
+	 * If the current node has no parent an exception will be thrown.
+	 * 
+	 * @return
+	 * @throws RuntimeException if the current node has no parent node
+	 */
 	public GroupZipper up() {
 		if(parent == null) {
 			throw new RuntimeException("Could not move up because this group does not have a parent");
@@ -42,36 +74,78 @@ public class GroupZipper {
 		return parent;
 	}
 	
+	/**
+	 * Navigates right the tree to the next node at the same level.
+	 * 
+	 * @return
+	 */
 	public GroupZipper right() {
+		if(index + 1 >= parent.getNode().getGroups().size()) {
+			throw new RuntimeException("Could not move right because the last node at this level has already been reached");
+		}
+		
 		index++;
 		node = parent.getNode().getGroups().get(index);
 		
 		return this;
 	}
 	
+	/**
+	 * Navigates left the tree to the previous node at the same level.
+	 * 
+	 * @return
+	 */
 	public GroupZipper left() {
+		if(index - 1 < 0) {
+			throw new RuntimeException("Could not move left because the first node at this level has already been reached");
+		}
+		
 		index--;
 		node = parent.getNode().getGroups().get(index);
 		
 		return this;
 	}
 	
+	/**
+	 * Returns the current node. This can be seen as a pointer to the current element in the tree.
+	 * <p>
+	 * 
+	 * @return
+	 */
 	public Group getNode() {
 		return node;
 	}
 
+	/**
+	 * Replaces the current node with the given one.
+	 * <p>
+	 * Can be used to modify the tree.
+	 * 
+	 * @param group the replacement node
+	 * @return
+	 */
 	public GroupZipper replace(Group group) {
 		parent.getNode().getGroups().set(index, group);
 		
 		return this;
 	}
 	
+	/**
+	 * Returns a new {@link KeePassFile} from the current tree structure.
+	 * 
+	 * @return a new KeePass file
+	 */
 	public KeePassFile close() {
 		Group rootNode = getRoot();
 		
 		return new KeePassFileBuilder(meta).addTopGroups(rootNode).build();
 	}
 	
+	/**
+	 * Returns the root node of the tree.
+	 * 
+	 * @return the root node of the tree
+	 */
 	public Group getRoot() {
 		if(parent == null) {
 			return node;
--- src/test/java/de/slackspace/openkeepass/domain/GroupZipperTest.java
+++ src/test/java/de/slackspace/openkeepass/domain/GroupZipperTest.java
@@ -1,7 +1,9 @@
 package de.slackspace.openkeepass.domain;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import de.slackspace.openkeepass.domain.zipper.GroupZipper;
 
@@ -49,5 +51,75 @@ public class GroupZipperTest {
 		KeePassFile keePassFileModified = zipper.close();
 		Assert.assertNotNull(keePassFileModified.getGroupByName("Fashion"));
 		Assert.assertNotNull(keePassFileModified.getEntryByTitle("Entry #2"));
+	}
+	
+	@Test
+	public void shouldNavigateThroughTreeLeftAndRightWithoutProblems() {
+		KeePassFile keePassFile = createFlatGroupStructure();
+		
+		GroupZipper zipper = new GroupZipper(keePassFile);
+		Assert.assertEquals("C", zipper.down().right().right().getNode().getName());
+		Assert.assertEquals("A", zipper.left().left().getNode().getName());
+	}
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	@Test
+	public void shouldThrowExeptionWhileMovingTooFarRight() {
+		KeePassFile keePassFile = createFlatGroupStructure();
+		
+		GroupZipper zipper = new GroupZipper(keePassFile).down().right().right();
+		
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage("Could not move right because the last node at this level has already been reached");
+		zipper.right();
+	}
+	
+	@Test
+	public void shouldThrowExeptionWhileMovingTooFarLeft() {
+		KeePassFile keePassFile = createFlatGroupStructure();
+		
+		GroupZipper zipper = new GroupZipper(keePassFile).down();
+		
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage("Could not move left because the first node at this level has already been reached");
+		zipper.left();
+	}
+	
+	@Test
+	public void shouldThrowExeptionWhileMovingDownWithoutChilds() {
+		KeePassFile keePassFile = createFlatGroupStructure();
+		
+		GroupZipper zipper = new GroupZipper(keePassFile).down();
+		
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage("Could not move down because this group does not have any children");
+		zipper.down();
+	}
+	
+	@Test
+	public void shouldThrowExeptionWhileMovingUpWithoutParent() {
+		KeePassFile keePassFile = createFlatGroupStructure();
+		
+		GroupZipper zipper = new GroupZipper(keePassFile);
+		
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage("Could not move up because this group does not have a parent");
+		zipper.up();
+	}
+
+	private KeePassFile createFlatGroupStructure() {
+		Group root = new GroupBuilder()
+				.addGroup(new GroupBuilder("A").build())
+				.addGroup(new GroupBuilder("B").build())
+				.addGroup(new GroupBuilder("C").build())
+				.build();
+		
+		KeePassFile keePassFile = new KeePassFileBuilder("writeTreeDB")
+				.addTopGroups(root)
+				.build();
+		
+		return keePassFile;
 	}
 }
