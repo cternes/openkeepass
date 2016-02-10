e6c77e7200ad51b0fd625a785c2511b53587f7a0
cternes <github@slackspace.de>
2016-02-10 11:44:48 +0100

    Added test for readInt

--- src/test/java/de/slackspace/openkeepass/util/ByteUtilsTest.java
+++ src/test/java/de/slackspace/openkeepass/util/ByteUtilsTest.java
@@ -2,6 +2,8 @@ package de.slackspace.openkeepass.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.UUID;
 
 import org.junit.Test;
@@ -16,5 +18,16 @@ public class ByteUtilsTest {
 		UUID uuid2 = ByteUtils.bytesToUUID(bytes);
 
 		assertEquals(uuid, uuid2);
+	}
+	
+	@Test
+	public void shouldReadInt() throws IOException {
+		int i = 200;
+		byte[] bytes = new byte[] {(byte) i};
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+		
+		int readInt = ByteUtils.readInt(inputStream);
+		
+		assertEquals(200, readInt);
 	}
 }
