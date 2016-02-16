eb833faff77b01968decd750242f2cd595d33348
cternes <github@slackspace.de>
2016-02-16 21:22:10 +0100

    Added random generator test

---
+++ src/test/java/de/slackspace/openkeepass/crypto/RandomGeneratorTest.java
@@ -0,0 +1,23 @@
+package de.slackspace.openkeepass.crypto;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RandomGeneratorTest {
+
+	@Test
+	public void shouldGeneratorOneRandomByte() {
+		RandomGenerator randomGenerator = new RandomGenerator();
+		byte[] randomBytes = randomGenerator.getRandomBytes(1);
+		Assert.assertNotNull(randomBytes);
+		Assert.assertEquals(randomBytes.length, 1);
+	}
+
+	@Test
+	public void shouldGeneratorFourRandomBytes() {
+		RandomGenerator randomGenerator = new RandomGenerator();
+		byte[] randomBytes = randomGenerator.getRandomBytes(4);
+		Assert.assertNotNull(randomBytes);
+		Assert.assertEquals(randomBytes.length, 4);
+	}
+}
