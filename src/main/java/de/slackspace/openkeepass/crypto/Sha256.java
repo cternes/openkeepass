5a723524cd4cd105c3b66cd0960b28e7d614cf0f
cternes <github@slackspace.de>
2016-03-09 18:07:28 +0100

    Simplified method in HashedBlockOutputStream

--- src/main/java/de/slackspace/openkeepass/crypto/Sha256.java
+++ src/main/java/de/slackspace/openkeepass/crypto/Sha256.java
@@ -7,7 +7,7 @@ import java.security.NoSuchAlgorithmException;
 public class Sha256 {
 
 	private Sha256() {}
-	
+
 	public static byte[] hash(String text) {
 		if (text == null) {
 			throw new IllegalArgumentException("Text must not be null");
@@ -25,9 +25,17 @@ public class Sha256 {
 			throw new IllegalArgumentException("Bytes must not be null");
 		}
 
+		return hash(bytes, 0, bytes.length);
+	}
+
+	public static byte[] hash(byte[] bytes, int offset, int length) {
+		if (bytes == null) {
+			throw new IllegalArgumentException("Bytes must not be null");
+		}
+
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			md.update(bytes);
+			md.update(bytes, offset, length);
 			return md.digest();
 		} catch (NoSuchAlgorithmException e) {
 			throw new UnsupportedOperationException("The algorithm 'SHA-256' is not supported", e);
