7991fe70d9e7698e8ff07110e7a79473ef2095c1
cternes <github@slackspace.de>
2016-04-19 18:07:00 +0200

    Added KeyFileUnreadableException

---
+++ src/main/java/de/slackspace/openkeepass/exception/KeyFileUnreadableException.java
@@ -0,0 +1,13 @@
+package de.slackspace.openkeepass.exception;
+
+public class KeyFileUnreadableException extends RuntimeException {
+
+	public KeyFileUnreadableException(String message) {
+		super(message);
+	}
+
+	public KeyFileUnreadableException(String message, Throwable e) {
+		super(message, e);
+	}
+
+}
