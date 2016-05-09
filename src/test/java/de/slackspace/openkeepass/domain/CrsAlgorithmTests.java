a12d216bb84de3b94ddcc533f2d5f47e880c8bff
cternes <github@slackspace.de>
2016-05-09 17:44:50 +0200

    Added test for CrsAlgorithm

---
+++ src/test/java/de/slackspace/openkeepass/domain/CrsAlgorithmTests.java
@@ -0,0 +1,33 @@
+package de.slackspace.openkeepass.domain;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+public class CrsAlgorithmTests {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldParseValueAndThrowException() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Value 1324 is not a valid CrsAlgorithm");
+
+        CrsAlgorithm.parseValue(1324);
+    }
+
+    @Test
+    public void shouldParseValue() {
+        CrsAlgorithm crsAlgorithm = CrsAlgorithm.parseValue(1);
+        Assert.assertEquals(CrsAlgorithm.ArcFourVariant, crsAlgorithm);
+    }
+
+    @Test
+    public void shouldGetIntValue() {
+        int intValue = CrsAlgorithm.getIntValue(CrsAlgorithm.Salsa20);
+        Assert.assertEquals(2, intValue);
+    }
+}
