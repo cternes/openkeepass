5675ec2166a4896d1f3590ea19ea218d8c928b53
cternes <github@slackspace.de>
2016-05-09 17:46:41 +0200

    Added test for CompressionAlgorithm

---
+++ src/test/java/de/slackspace/openkeepass/domain/CompressionAlgorithmTests.java
@@ -0,0 +1,33 @@
+package de.slackspace.openkeepass.domain;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+public class CompressionAlgorithmTests {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void shouldParseValueAndThrowException() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Value 1324 is not a valid CompressionAlgorithm");
+
+        CompressionAlgorithm.parseValue(1324);
+    }
+
+    @Test
+    public void shouldParseValue() {
+        CompressionAlgorithm compressionAlgorithm = CompressionAlgorithm.parseValue(1);
+        Assert.assertEquals(CompressionAlgorithm.Gzip, compressionAlgorithm);
+    }
+
+    @Test
+    public void shouldGetIntValue() {
+        int intValue = CompressionAlgorithm.getIntValue(CompressionAlgorithm.Gzip);
+        Assert.assertEquals(1, intValue);
+    }
+}
