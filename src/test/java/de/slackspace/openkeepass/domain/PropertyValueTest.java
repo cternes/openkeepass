cf6683efc5c04bdd2090a7b98b752651cd2e94c7
cternes <github@slackspace.de>
2016-07-20 18:56:32 +0200

    Added test for unmarshalling propertyvalue

--- src/test/java/de/slackspace/openkeepass/domain/PropertyValueTest.java
+++ src/test/java/de/slackspace/openkeepass/domain/PropertyValueTest.java
@@ -1,5 +1,6 @@
 package de.slackspace.openkeepass.domain;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 import org.junit.Assert;
@@ -18,6 +19,17 @@ public class PropertyValueTest {
         
         String xml = XmlStringCleaner.cleanXmlString(new String(bos.toByteArray()));
         Assert.assertEquals("<propertyValue Protected='False'>TestValue</propertyValue>", xml);
+    }
+    
+    @Test
+    public void shouldUnmarshallXmlToObject() throws Exception {
+        PropertyValue propertyValue = new PropertyValue(false, "TestValue");
+
+        String xml = "<propertyValue Protected='False'>TestValue</propertyValue>";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(xml.getBytes());
+        PropertyValue propertyValueUnmarshalled = new SimpleXmlParser().fromXml(inputStream, PropertyValue.class);
+
+        Assert.assertEquals(propertyValue.getValue(), propertyValueUnmarshalled.getValue());
     }
     
 }
